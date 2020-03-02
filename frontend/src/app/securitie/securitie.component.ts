8312270e292075244d2715e6ad7d054435597998
Nikunj Patodiya <nikunj.patodiya@flexiloans.com>
2020-03-02 09:50:50 +0530

    small changes

--- frontend/src/app/securitie/securitie.component.ts
+++ frontend/src/app/securitie/securitie.component.ts
@@ -19,6 +19,7 @@ export class SecuritieComponent implements OnInit {
   public toDayDate = new Date('2004-11-30');
   public portfolioId: number;
   public apiCallActive = true;
+  public totalSharevalue = 0;
   public myOptions: IAngularMyDpOptions = {
     dateRange: false,
     dateFormat: 'dd-mm-yyyy'
@@ -28,7 +29,7 @@ export class SecuritieComponent implements OnInit {
   @ViewChild(DatatableComponent, { static: false }) table: DatatableComponent;
   constructor(public mainService: MainService, public router: Router, public route: ActivatedRoute) {
     this.createPortfoliaoForm();
-    this.route.paramMap.subscribe((params: ParamMap) =>  {
+    this.route.paramMap.subscribe((params: ParamMap) => {
       this.portfolioId = +params.get('id');
       if (this.portfolioId) {
         this.getSecurity();
@@ -44,16 +45,24 @@ export class SecuritieComponent implements OnInit {
       portfoliaoDate: moment(this.portfoliaoDate.singleDate.formatted, 'DD-mm-YYYY').format('YYYY-mm-DD') as string
     };
     this.apiCallActive = true;
+    this.totalSharevalue = 0;
     this.mainService.portfolioByIdDate(body).subscribe((response: PortfolioByDateResponse) => {
       this.portfoliao = response;
       if (this.portfoliao.Transactions) {
         this.transactionsList = response.Transactions as TransactionList[];
+        this.transactionsList.forEach(item => {
+          if (item.Type === 'Buy' && item.security && item.security.length > 0) {
+            this.totalSharevalue = this.totalSharevalue + item.security[0].Amount;
+          }
+        });
       } else {
         this.transactionsList = [] as TransactionList[];
+        this.totalSharevalue = 0;
       }
       this.apiCallActive = false;
     }, (error: any) => {
       this.apiCallActive = false;
+      this.totalSharevalue = 0;
     });
   }
   toggleExpandRow(row) {
@@ -72,7 +81,7 @@ export class SecuritieComponent implements OnInit {
     }
   }
   createPortfoliaoForm() {
-    this.portfoliaoDate = {isRange: false, singleDate: {jsDate: this.toDayDate, formatted: moment(this.toDayDate).format('DD-MM-YYYY') as string }, dateRange: null} as  IMyDateModel;
+    this.portfoliaoDate = { isRange: false, singleDate: { jsDate: this.toDayDate, formatted: moment(this.toDayDate).format('DD-MM-YYYY') as string }, dateRange: null } as IMyDateModel;
     this.portfoliaoDetail = new FormGroup({
       portfoliaoDate: new FormControl(this.portfoliaoDate, [Validators.required]),
     });
